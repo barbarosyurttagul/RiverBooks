c6baaf5559bcf47020e6be1dc1b81399e9e4749a
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2024-10-13 14:44:52 +0100

    implemented BooksModuleQuery mediatr

---
+++ src/Books Module/RiverBooks.Books/Contracts/BookDetailsQuery.cs
@@ -0,0 +1,28 @@
+using Ardalis.Result;
+using MediatR;
+
+namespace RiverBooks.Books.Contracts;
+
+public record BookDetailsQuery(Guid BookId) : IRequest<Result<BookDetailsResponse>>;
+
+internal class BookDetailsQueryHandler : IRequestHandler<BookDetailsQuery, Result<BookDetailsResponse>>
+{
+  private readonly IBookService _bookService;
+
+  public BookDetailsQueryHandler(IBookService bookService)
+  {
+    _bookService = bookService;
+  }
+
+  public async Task<Result<BookDetailsResponse>> Handle(BookDetailsQuery request, CancellationToken cancellationToken)
+  {
+    var book = await _bookService.GetByBookIdAsync(request.BookId);
+
+    if (book == null)
+    {
+      return Result.NotFound();
+    }
+
+    return new BookDetailsResponse(book.Id, book.Title, book.Author, book.Price);
+  }
+}
---
+++ src/Books Module/RiverBooks.Books/Contracts/BookDetailsResponse.cs
@@ -0,0 +1,7 @@
+namespace RiverBooks.Books.Contracts;
+
+public record BookDetailsResponse(Guid BookId, string Title,
+   string Author, decimal Price)
+{
+
+}
